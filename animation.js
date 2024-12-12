c4f8dcf80cc1ade7652d2b15280d6e134371b73d
Mac <mac@rendiero.local>
2024-12-12 22:42:05 +0700

    Install GSAP

---
+++ animation.js
@@ -0,0 +1,5 @@
+import {gsap} from 'gsap';
+//import {ScrollTrigger} from "gsap/ScrollTrigger";
+
+//gsap.registerPlugin(ScrollTrigger);
+gsap.to('.profile', {x:300, rotation:27, duration:2});
--- dev.js
+++ dev.js
@@ -1,6 +1,8 @@
+//mobile menu - start
 const menubutton = document.getElementById('menu-btn');
 const mobilemenu = document.getElementById('mobile-menu');
 
 menubutton.addEventListener('click', () => {
   mobilemenu.classList.toggle('hidden');
 });
+//mobile menu - end
